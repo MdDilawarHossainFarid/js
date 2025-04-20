bdd98a874641a2f25e6aa3b47adcad4a167dc211
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2025-04-20 17:36:39 +0530

    returning multiple value

---
+++ returning -multiple-value.js
@@ -0,0 +1,37 @@
+
+function getmultipleValue(){
+    const value1 = "Rose";
+    const value2 = "Sunflower";
+    const value3 = "Hydrangea";
+    const value4 = "Lily";
+
+    const arr = [1,2,3,4,];
+
+    return [value1,value2,value3,value4]
+    return arr;
+    return "FArid";
+    const person = {
+        name:"Farid",
+        city:"Beldanga"
+    }
+
+    return person;
+
+    return [1,2,3,4,5,6,7,8];
+
+}
+const returnValue = getmultipleValue();
+console.log(typeof returnValue);
+const [value1,value2,value3,value4] = getmultipleValue()
+console.log(value1)
+const data = getmultipleValue();
+const {name:a,city} = getmultipleValue()
+const []
+console.log(a);
+console.log(city);
+console.log(typeof data);
+console.log(value1);
+
+const [data,...rest] = getmultipleValue();
+console.log(data);
+console.log(rest);
